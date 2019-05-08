c6a8ddec4a35c866c6c8791fcb3bcbdeb23ef990
Jakub Kolodziejczyk <kuba@aiokinawa.com>
2019-05-08 09:13:46 +0900

    Stubbed out dummy prediction endpoint
    No tests for it yet

--- traffic/web/blueprint.py
+++ traffic/web/blueprint.py
@@ -3,8 +3,10 @@ Module with server endpoints
 """
 
 import datetime
+import json
 
 import flask
+import numpy as np
 
 
 BLUEPRINT = flask.blueprints.Blueprint(name='blueprint', import_name=__name__)
@@ -17,3 +19,24 @@ def ping():
     """
 
     return "ping at {}".format(datetime.datetime.utcnow())
+
+
+@BLUEPRINT.route("/dummy_top_prediction")
+def dummy_top_prediction():
+    """
+    Dummy prediction endpoint. Uses actual prediction model, but creates dummy input
+    """
+
+    with flask.current_app.default_graph.as_default():
+
+        data = np.random.rand(1, 32, 32, 3)
+        raw_predictions = flask.current_app.traffic_signs_model.predict(data)[0]
+
+        category_index = np.argmax(raw_predictions)
+
+        result_dictionary = {
+            "category_index": int(category_index),
+            "confidence": float(raw_predictions[category_index])
+        }
+
+        return json.dumps(result_dictionary)
--- traffic/web/server.py
+++ traffic/web/server.py
@@ -3,8 +3,27 @@ Module for building configurable flask app
 """
 
 import flask
+import tensorflow as tf
 
 import traffic.web.blueprint
+import traffic.ml
+
+
+def setup_prediction_models(app, config):
+    """
+    Adds prediction models to application object
+    :param app: flask application instance
+    :param config: dictionary with options
+    """
+
+    # Set up traffic signs categorization model
+    app.traffic_signs_model = traffic.ml.get_model(config["input_shape"], config["categories_count"])
+
+    if config["environment"] != "test":
+        print("\n\nWarning! Skipping loading weights! Need to sort this part out!\n\n".upper())
+        # app.traffic_signs_model.load_weights(filepath=config["positive_negative"]["best_model_weights_path"])
+
+    app.default_graph = tf.get_default_graph()
 
 
 def get_configured_web_app(config):
@@ -17,6 +36,7 @@ def get_configured_web_app(config):
     app = flask.Flask("traffic_signs")
     app.debug = config["server"]["run_in_debug_mode"]
 
+    setup_prediction_models(app, config)
     app.register_blueprint(traffic.web.blueprint.BLUEPRINT)
 
     return app
