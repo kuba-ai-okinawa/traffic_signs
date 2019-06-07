038f12c948086da9a97934a81fce23b306e3090c
shunsuke.nakamura <shunsuke.nakamura@dena.com>
2019-06-07 17:48:12 +0900

    local lintが通ったtop-prediction関数

--- scripts/run_server.py
+++ scripts/run_server.py
@@ -4,12 +4,12 @@ Script for running flask server
 
 import datetime
 
+import json
 import flask
 import numpy as np
 import pandas
 import tensorflow as tf
 import cv2
-import json
 
 import traffic.ml
 import traffic.utilities
@@ -35,12 +35,17 @@ GENERAL = flask.Blueprint('general', __name__)
 
 
 def compute_top_k_indexes(predicted: np.ndarray, k: int):
+    """
+    :param predicted: CNN outputs (confidence values)
+    :param k: number of top results
+    :return: top-k index list
+    """
     sorted_indexes = np.argsort(predicted)[::-1]
-
+    traffic_sign_categories = flask.current_app.traffic_signs_categories
     assert k > 0
 
-    if k > len(GENERAL.traffic_signs_categories):
-        k = len(GENERAL.traffic_signs_categories)
+    if k > len(traffic_sign_categories):
+        k = len(traffic_sign_categories)
 
     return sorted_indexes[:k]
 
@@ -59,8 +64,8 @@ def top_prediction():
     """
     Top prediction endpoint, outputs top prediction category and confidence
     """
-
-    with flask.current_app.default_graph.as_default():
+    app = flask.current_app
+    with app.default_graph.as_default():
         raw_image_file = flask.request.files["image"]
         image = traffic.utilities.binary_rgb_image_string_to_numpy_image(raw_image_file.read())
 
@@ -69,7 +74,7 @@ def top_prediction():
 
         # Magic herei
 
-        y = GENERAL.traffic_signs_model.predict(image)[0]
+        y = app.traffic_signs_model.predict(image)[0]
 
         top_1_dict = generate_top_k_dicts(y, 1)[0]
 
@@ -77,14 +82,26 @@ def top_prediction():
 
 
 def generate_top_k_dicts(predicted: np.ndarray, k: int):
+    """
+    generate top-k dictionaries from CNN output
+    :param predicted: CNN outputs (confidence values)
+    :param k: number of top results
+    :return: top-k list of dictionary
+    """
+    app = flask.current_app
     top_k_indexes = compute_top_k_indexes(predicted, k)
     return [{'rank': rank + 1,
-             'category': GENERAL.traffic_signs_categories[top_k_index],
+             'category': app.traffic_signs_categories[top_k_index],
              'confidence': float(predicted[top_k_index])}
             for rank, top_k_index in enumerate(top_k_indexes)]
 
 
 def preprocess(np_image: np.ndarray) -> np.ndarray:
+    """
+    convert np_image into acceptable shape and range for CNN
+    :param np_image: np.ndarray [W, H, C]
+    :return: np.ndarray [1, 32, 32, 3]
+    """
     resized_image = cv2.resize(np_image, (32, 32), cv2.INTER_LANCZOS4)
     resized_image = resized_image.astype(np.float32) / 255
     return resized_image[np.newaxis, :, :, :]
